8e97eda9c310aa71859b3c69988d78672d932b0e
Irmak Sirer <irmak.sirer@gmail.com>
2026-05-25 19:30:09 +0300

    Fixed Genius card's design: add 2 to development, add 1 influence

--- card.py
+++ card.py
@@ -68,8 +68,8 @@ class Military(StandardCard):
 @dataclass(frozen=True)
 class Genius(SpecialCard):
     def resolve(self, module: 'Module', player_idx: int, game: 'Game') -> None:
-        module.add_influence(player_idx, 2)
-        module.adjust_dev(1)
+        module.add_influence(player_idx, 1)
+        module.adjust_dev(2)
 
 
 @dataclass(frozen=True)
